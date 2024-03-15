8ed81c38a6ccc9f85ecdf6330d0f9aba89c48977
spirit1086 <arman.mambetov@erg.kz>
2024-03-15 17:36:33 +0500

    feat: wallet module

---
+++ app/Modules/Wallet/Dto/WalletDto.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Modules\Wallet\Dto;
+
+class WalletDto
+{
+    private int $walletID;
+    private string $transactioncType;
+    private float $balance;
+}
--- config/modules.php
+++ config/modules.php
@@ -2,6 +2,7 @@
 return [
     'modules' =>[
         'Rate',
-        'ApiServices'
+        'ApiServices',
+        'Wallet'
     ]
 ];
