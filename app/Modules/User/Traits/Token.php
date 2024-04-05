e82a1cef9c056d27c0b71b5aa852a1d061c69ee3
spirit1086 <spirit1086@gmail.com>
2024-04-05 15:32:52 +0500

    feat: get user from token

--- app/Modules/User/Traits/Token.php
+++ app/Modules/User/Traits/Token.php
@@ -7,7 +7,7 @@ use Laravel\Sanctum\PersonalAccessToken;
 
 trait Token
 {
-   public static function getUser(Request $request)
+   public static function getUserFromToken(Request $request)
    {
        $token = PersonalAccessToken::findToken($request->bearerToken());
        return $token->tokenable;
